70bdce1d464f7b24124017b96de971c533d3a7b4
TooAnonymous <131053442+TooAnonymous@users.noreply.github.com>
2025-04-29 20:46:17 -0500

    Added frontend web console abilities to NWS

--- backend/kafka_service/kafka_ws_bridge.py
+++ backend/kafka_service/kafka_ws_bridge.py
@@ -9,6 +9,7 @@ connected_clients = set()
 def get_consumer():
     return KafkaConsumer(
         'GFW',
+        'NWS',
         bootstrap_servers='localhost:9092',
         auto_offset_reset='latest',
         enable_auto_commit=True,
