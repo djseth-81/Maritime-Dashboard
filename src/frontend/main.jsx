9fa1949eba9e40fac4d2ac61c0b21ffd9af8eaee
djseth-81 <djseth.81@proton.me>
2025-05-05 23:52:41 -0500

    Disabled Login for app

--- src/frontend/main.jsx
+++ src/frontend/main.jsx
@@ -19,22 +19,22 @@ const Layout = () => {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: <App />,
     errorElement: <ErrorPage />,
-    children: [
-      {
-        index: "/",
-        element: <ProtectedRoute component={<App/>} isAuthRequired={true}/>,
-      },
-      {
-        path: "login",
-        element: <ProtectedRoute component={<Auth/>} isAuthRequired={false}/>,
-      },
-      {
-        path: "register",
-        element: <ProtectedRoute component={<Auth/>} isAuthRequired={false}/>,
-      },
-    ],
+    // children: [
+      // {
+      //   path: "App",
+      //   element: <ProtectedRoute component={<App/>} isAuthRequired={false}/>,
+      // },
+      // {
+      //   path: "login",
+      //   element: <ProtectedRoute component={<Auth/>} isAuthRequired={false}/>,
+      // },
+      // {
+      //   path: "register",
+      //   element: <ProtectedRoute component={<Auth/>} isAuthRequired={false}/>,
+      // },
+    // ],
   },
 ]);
 
